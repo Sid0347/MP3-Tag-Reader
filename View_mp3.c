0c57be653583f19abd815af4ea83e3690c2205d9
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-14 19:33:59 +0000

    Remove position check

--- View_mp3.c
+++ View_mp3.c
@@ -4,41 +4,40 @@
 const char *TAGS[] = {"TIT2", "TPE1", "TALB", "TYER", "TCON", "COMM"};
 const char *Lables[] = {"Title       :   ", "Artist      :   ", "Album       :   ", "Year        :   ", "Content     :   ", "Comment     :   "};
 const int TAGS_Count = 6;
-int i = 0, Count = 0;
+int i;
+int Temp_Count; /* For while loop termination.*/
 
 unsigned char header[10];
 void View_Song_Details(char *File_Name)
 {
     printf("|-------------------SELECTED VIEW DETAILS--------------------|\n");
     printf("|------------------------------------------------------------|\n");
-    printf("            MP3 TAG READER AND EDITOR FOR ID3v2             \n");
+    printf("            MP3 TAG READER AND EDITOR FOR ID3v2               \n");
     printf("|------------------------------------------------------------|\n");
-    while (Count != TAGS_Count)
+    while (Temp_Count != TAGS_Count)
     {
         fread(header, 1, 10, fptr);
-        unsigned int Data_size = Convert_Size(header + 4);
-        unsigned char *Tag_Data = malloc(Data_size + 1);
+        unsigned int Data_Size = Convert_Size(header + 4);
+        unsigned char *Tag_Data = malloc(Data_Size + 1);
         if (!Tag_Data)
         {
             perror("malloc failed");
             exit(1);
         }
-        fread(Tag_Data, 1, Data_size, fptr);
+        fread(Tag_Data, 1, Data_Size, fptr);
 
         /* Check for tag*/
         if (memcmp(header, TAGS[i], 4) == 0)
         {
             printf("%s", Lables[i]);
-            fwrite(Tag_Data, 1, Data_size, stdout);
+            fwrite(Tag_Data, 1, Data_Size, stdout);
             printf("\n");
             i++;
         }
-        Count++;
+        Temp_Count++;
     }
     printf("|------------------------------------------------------------|\n");
-    int pos = 0;
-    pos = ftell(fptr);
-    printf("position %d\n", pos);
+    
 }
 
 /* Function to convert 4-byte big-endian value from file into little-endian.*/
