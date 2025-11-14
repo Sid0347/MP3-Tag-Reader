d0f26ab171d0ef7357ab6f1253be7070befef7c8
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-14 19:35:39 +0000

    Add edit function prototype and add some variable declarations with extern

--- Tag_Header.h
+++ Tag_Header.h
@@ -3,11 +3,16 @@
 #include <string.h>
 
 extern FILE *fptr;
+extern FILE *tptr;
 
 /* Array of string tags.*/
 extern const char *TAGS[];
 extern const int TAGS_Count;
+extern unsigned char header[];
+extern unsigned int Data_Size;
+extern unsigned char Tag_Data;
 
 /* Function Prototypes.*/
 void View_Song_Details(char *File_Name);
+void Edit_Song_Details(char *Tag, char *Edit_Data, char *File_Name);
 unsigned int Convert_Size(const unsigned char size_bytes[4]);
