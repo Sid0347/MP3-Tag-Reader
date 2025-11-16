149cb49700fd63c5bac756b60d760851a654ffbd
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-16 13:17:42 +0000

    Add Tag_Count macro

--- Tag_Header.h
+++ Tag_Header.h
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAGS_Count 6
+
 extern FILE *fptr;
 extern FILE *tptr;
 
 /* Array of string tags.*/
 extern const char *TAGS[];
-extern const int TAGS_Count;
 extern unsigned char header[];
 extern unsigned int Data_Size;
-extern unsigned char Tag_Data;
+extern unsigned char *Tag_Data;
 
 /* Function Prototypes.*/
 void View_Song_Details(char *File_Name);
 void Edit_Song_Details(char *Tag, char *Edit_Data, char *File_Name);
 unsigned int Convert_Size(const unsigned char size_bytes[4]);
+long Remaining_Bytes(FILE *fptr);
