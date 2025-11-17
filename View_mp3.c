e66e24002eca10620e8632b25a8bf55d80977577
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-17 17:28:49 +0000

    Remove logic of skip encoding byte

--- View_mp3.c
+++ View_mp3.c
@@ -3,11 +3,11 @@
 /* Tags to check readed tag are correct or not*/
 const char *TAGS[] = {"TIT2", "TPE1", "TALB", "TYER", "TCON", "COMM"};
 const char *Lables[] = {"Title       :   ", "Artist      :   ", "Album       :   ", "Year        :   ", "Content     :   ", "Comment     :   "};
-unsigned int Data_Size;
+uint32_t Data_Size;
 unsigned char *Tag_Data;
 unsigned char header[10];
 
-void View_Song_Details(char *File_Name)
+void view_song_details(char *File_Name)
 {
     printf("|-------------------SELECTED VIEW DETAILS--------------------|\n");
     printf("|------------------------------------------------------------|\n");
@@ -16,30 +16,65 @@ void View_Song_Details(char *File_Name)
     int i = 0;
     while (i != TAGS_Count) /* while loop terminate when i = 6.*/
     {
-        fread(header, 1, 10, fptr);
-        Data_Size = Convert_Size(header + 4);
-        Tag_Data = malloc(Data_Size + 1);
+        if (fread(header, 1, 10, fptr) != 10) /* Stop if header reach EOF or read is corrupt.*/
+            break;
+
+        Data_Size = convert_little(header + 4);
+        if (Data_Size == 0) /* If data size comes zero then avoid malloc(0).*/
+            break;
+        Tag_Data = malloc(Data_Size);
         if (!Tag_Data)
         {
-            perror("malloc failed");
+            perror("malloc failed in View");
             exit(1);
         }
-        fread(Tag_Data, 1, Data_Size, fptr);
+
+        if (fread(Tag_Data, 1, Data_Size, fptr) != Data_Size) /* Stop if data not fully read.*/
+        {
+            free(Tag_Data);
+            break;
+        }
 
         /* Check for tag*/
-        if (memcmp(header, TAGS[i], 4) == 0)
+        for (int j = 0; j < TAGS_Count; j++)
         {
-            printf("%s", Lables[i]);
-            fwrite(Tag_Data, 1, Data_Size, stdout);
-            printf("\n");
-            i++;
+            if (memcmp(header, TAGS[j], 4) == 0)
+            {
+                printf("%s", Lables[j]);
+                fwrite(Tag_Data, 1, Data_Size, stdout);
+                printf("\n");
+
+                if (j == i)
+                    i++;
+                break;
+            }
         }
+        free(Tag_Data);
     }
     printf("|------------------------------------------------------------|\n");
 }
 
-/* Function to convert 4-byte big-endian value from file into little-endian.*/
-unsigned int Convert_Size(const unsigned char size_bytes[4])
+/* Error and usage message if argument is equal or less than one.*/
+void error_and_usage()
 {
-    return (size_bytes[0] << 24) | (size_bytes[1] << 16) | (size_bytes[2] << 8) | size_bytes[3];
+    printf("------------------------------------------------------------------------------------------\n");
+    printf("ERROR : ./a.out : INVALID ARGUMENTS\n");
+    printf("To view please pass like : ./a.out -v mp3filename\n");
+    printf("To edit please pass like : ./a.out -e (-t/ -a/ -A/ -m/ -y/ -c) changing_text mp3filename\n");
+    printf("To get help pass like : ./a.out --help\n");
+    printf("------------------------------------------------------------------------------------------\n");
+}
+
+/* The help menu (Show detailed meaning of argument.)*/
+void help_menu()
+{
+    printf("--------------------HELP MENU--------------------\n");
+    printf("1. -v -> To view mp3 file contents\n2. -e -> To edit mp3 file contents\n");
+    printf("    2.1] -t -> To edit song title\n");
+    printf("    2.2] -a -> To edit artist name\n");
+    printf("    2.3] -A -> To edit album name\n");
+    printf("    2.4] -y -> To edit year\n");
+    printf("    2.5] -c -> To edit content\n");
+    printf("    2.6] -m -> To edit comment\n");
+    printf("-------------------------------------------------\n");
 }
