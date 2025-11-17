acb6ee1799a3a5014ebeb11d226ae9f85d37561f
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-17 17:26:21 +0000

    Re-edit function names in lower case as according to coding standards says

--- Tag_Header.h
+++ Tag_Header.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define TAGS_Count 6
 
@@ -14,7 +15,10 @@ extern unsigned int Data_Size;
 extern unsigned char *Tag_Data;
 
 /* Function Prototypes.*/
-void View_Song_Details(char *File_Name);
-void Edit_Song_Details(char *Tag, char *Edit_Data, char *File_Name);
-unsigned int Convert_Size(const unsigned char size_bytes[4]);
-long Remaining_Bytes(FILE *fptr);
+void error_and_usage();
+void help_menu();
+void view_song_details(char *File_Name);
+void edit_song_details(char *Tag, char *Edit_Data, char *File_Name);
+uint32_t convert_little(const unsigned char size_bytes[4]);
+uint32_t to_big_endian(uint32_t Edit_Size, unsigned char Edit_Bytes[4]);
+long remaining_bytes(FILE *fptr);
